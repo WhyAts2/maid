c0eca11cb33161919fdfa827d12dad018f63692e
WhyAts2 <74931466+WhyAts2@users.noreply.github.com>
2021-01-27 16:43:57 +0300

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -12,25 +12,25 @@ const fs = require('fs');
 module.exports = {
 	info: {
 		name: "playlist",
-		description: "To play songs :D",
+		description: "Şarkı çalmak için :D",
 		usage: "<YouTube Playlist URL | Playlist Name>",
 		aliases: ["pl"],
 	},
 
 	run: async function (client, message, args) {
 		const channel = message.member.voice.channel;
-		if (!channel) return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+		if (!channel) return sendError("Üzgünüm ama müzik çalmak için bir ses kanalında olmanız gerekiyor!", message.channel);
 		const url = args[0] ? args[0].replace(/<(.+)>/g, "$1") : "";
 		var searchString = args.join(" ");
 		const permissions = channel.permissionsFor(message.client.user);
-		if (!permissions.has("CONNECT")) return sendError("I cannot connect to your voice channel, make sure I have the proper permissions!", message.channel);
-		if (!permissions.has("SPEAK")) return sendError("I cannot speak in this voice channel, make sure I have the proper permissions!", message.channel);
+		if (!permissions.has("CONNECT")) return sendError("Ses kanalınıza bağlanamıyorum, uygun izinlere sahip olduğumdan emin olun !", message.channel);
+		if (!permissions.has("SPEAK")) return sendError("Bu ses kanalında konuşamıyorum, uygun izinlere sahip olduğumdan emin olun !", message.channel);
 
 		if (!searchString||!url) return sendError(`Usage: ${process.env.PREFIX}playlist <YouTube Playlist URL | Playlist Name>`, message.channel);
 		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
 			try {
 				const playlist = await ytpl(url.split("list=")[1]);
-				if (!playlist) return sendError("Playlist not found", message.channel)
+				if (!playlist) return sendError("Playlist bulunamadı", message.channel)
 				const videos = await playlist.items;
 				for (const video of videos) {
 					// eslint-disable-line no-await-in-loop
@@ -39,18 +39,18 @@ module.exports = {
 				return message.channel.send({
 					embed: {
 						color: "GREEN",
-						description: `✅  **|**  Playlist: **\`${videos[0].title}\`** has been added to the queue`
+						description: `✅  **|**  Playlist: **\`${videos[0].title}\`** sıraya eklendi`
 					}
 				})
 			} catch (error) {
 				console.error(error);
-				return sendError("Playlist not found :(",message.channel).catch(console.error);
+				return sendError("Playlist bulunamadı :(",message.channel).catch(console.error);
 			}
 		} else {
 			try {
 				var searched = await yts.search(searchString)
 
-				if (searched.playlists.length === 0) return sendError("Looks like i was unable to find the playlist on YouTube", message.channel)
+				if (searched.playlists.length === 0) return sendError("Görünüşe göre YouTube'da oynatma listesini bulamadım", message.channel)
 				var songInfo = searched.playlists[0];
 				let listurl = songInfo.listId;
 				const playlist = await ytpl(listurl)
@@ -60,13 +60,13 @@ module.exports = {
 					await handleVideo(video, message, channel, true); // eslint-disable-line no-await-in-loop
 				}
 				let thing = new MessageEmbed()
-					.setAuthor("Playlist has been added to queue", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+					.setAuthor("Playlist sıraya eklendi", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
 					.setThumbnail(songInfo.thumbnail)
 					.setColor("GREEN")
-					.setDescription(`✅  **|**  Playlist: **\`${songInfo.title}\`** has been added \`${songInfo.videoCount}\` video to the queue`)
+					.setDescription(`✅  **|**  Playlist: **\`${songInfo.title}\`** eklendi \`${songInfo.videoCount}\` video eklendi `)
 				return message.channel.send(thing)
 			} catch (error) {
-				return sendError("An unexpected error has occurred",message.channel).catch(console.error);
+				return sendError("Beklenmeyen bir hata oluştu",message.channel).catch(console.error);
 			}
 		}
 
@@ -109,13 +109,13 @@ module.exports = {
 				serverQueue.songs.push(song);
 				if (playlist) return;
 				let thing = new MessageEmbed()
-					.setAuthor("Song has been added to queue", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+					.setAuthor("Şarkı sıraya eklendi", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
 					.setThumbnail(song.img)
 					.setColor("YELLOW")
-					.addField("Name", song.title, true)
-					.addField("Duration", song.duration, true)
-					.addField("Requested by", song.req.tag, true)
-					.setFooter(`Views: ${song.views} | ${song.ago}`)
+					.addField("Ad", song.title, true)
+					.addField("Uzunluk", song.duration, true)
+					.addField("Tarafından istendi:", song.req.tag, true)
+					.setFooter(`Görüntülenme: ${song.views} | ${song.ago}`)
 				return message.channel.send(thing);
 			}
 			return;
@@ -130,7 +130,7 @@ async	function play(guild, song) {
     var online = afk[message.guild.id]
     if (!song){
       if (!online.afk) {
-        sendError("Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel run `!afk`", message.channel)
+        sendError("Sırada şarkı olmadığını düşündüğüm için ses kanalından ayrılıyorum. Botun 7/24 seslide kalmasını istiyorsan `!afk` komutunu kullan", message.channel)
         message.guild.me.voice.channel.leave();//If you want your bot stay in vc 24/7 remove this line :D
         message.client.queue.delete(message.guild.id);
       }
@@ -145,7 +145,7 @@ stream.on('error', function(er)  {
         if (serverQueue) {
         serverQueue.songs.shift();
         play(guild, serverQueue.songs[0]);
-  	  return sendError(`An unexpected error has occurred.\nPossible type \`${er}\``, message.channel)
+  	  return sendError(`Beklenmeyen bir hata oluştu.\nOlası tip \`${er}\``, message.channel)
 
          }
        }
