dd4031e214b476fa86d07351ffc4c3564896baba
WhyAts2 <74931466+WhyAts2@users.noreply.github.com>
2021-01-27 16:17:21 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   info: {
     name: "invite",
-    description: "To add/invite the bot to your server",
+    description: "Botun davet linki",
     usage: "[invite]",
     aliases: ["inv"],
   },
@@ -12,7 +12,7 @@ module.exports = {
     
     let invite = new MessageEmbed()
     .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
+    .setDescription(`Beni sunucunuzda ister misiniz? Beni bugün davet edin! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
     .setColor("BLUE")
     return message.channel.send(invite);
   },
