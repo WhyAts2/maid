0c57e8e4a5d054435b82e209008e1897172a8701
WhyAts2 <74931466+WhyAts2@users.noreply.github.com>
2021-01-27 16:23:19 +0300

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -4,23 +4,23 @@ const sendError = require("../util/error")
 module.exports = {
   info: {
     name: "nowplaying",
-    description: "To show the music which is currently playing in this server",
+    description: "Bu sunucuda şu anda çalan müziği gösterir",
     usage: "",
     aliases: ["np"],
   },
 
   run: async function (client, message, args) {
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+    if (!serverQueue) return sendError("Bu sunucuda oynatılan hiçbir şey yok.", message.channel);
     let song = serverQueue.songs[0]
     let thing = new MessageEmbed()
-      .setAuthor("Now Playing", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+      .setAuthor("Şuanda Oynatılıyor", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
       .setThumbnail(song.img)
       .setColor("BLUE")
-      .addField("Name", song.title, true)
-      .addField("Duration", song.duration, true)
-      .addField("Requested by", song.req.tag, true)
-      .setFooter(`Views: ${song.views} | ${song.ago}`)
+      .addField("Ad", song.title, true)
+      .addField("Uzunluk", song.duration, true)
+      .addField("Tarafından istendi:", song.req.tag, true)
+      .setFooter(`Görüntülenme: ${song.views} | ${song.ago}`)
     return message.channel.send(thing)
   },
 };
