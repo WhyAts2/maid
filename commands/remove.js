2b7705069d6b013384d412fa59f271c63a45a2e5
WhyAts2 <74931466+WhyAts2@users.noreply.github.com>
2021-01-27 16:57:00 +0300

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -4,25 +4,25 @@ const sendError = require("../util/error");
 module.exports = {
   info: {
     name: "remove",
-    description: "Remove song from the queue",
+    description: "Şarkıyı sıradan kaldırır",
     usage: "rm <number>",
     aliases: ["rm"],
   },
 
   run: async function (client, message, args) {
    const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return sendError("There is no queue.",message.channel).catch(console.error);
-    if (!args.length) return sendError(`Usage: ${process.env.PREFIX}\`remove <Queue Number>\``);
-    if (isNaN(args[0])) return sendError(`Usage: ${process.env.PREFIX}\`remove <Queue Number>\``);
-    if (queue.songs.length == 1) return sendError("There is no queue.",message.channel).catch(console.error);
+    if (!queue) return sendError("Sıra yok.",message.channel).catch(console.error);
+    if (!args.length) return sendError(`Kullanım: ${process.env.PREFIX}\`remove <Queue Number>\``);
+    if (isNaN(args[0])) return sendError(`Kullanım: ${process.env.PREFIX}\`remove <Queue Number>\``);
+    if (queue.songs.length == 1) return sendError("Sıra yok.",message.channel).catch(console.error);
     if (args[0] > queue.songs.length)
-      return sendError(`The queue is only ${queue.songs.length} songs long!`,message.channel).catch(console.error);
+      return sendError(`Sıra ${queue.songs.length} şarkı uzunluğunda!`,message.channel).catch(console.error);
 try{
     const song = queue.songs.splice(args[0] - 1, 1); 
-    sendError(`❌ **|** Removed: **\`${song[0].title}\`** from the queue.`,queue.textChannel).catch(console.error);
+    sendError(`❌ **|** Kaldırıldı: **\`${song[0].title}\`** sıradan kaldırıldı.`,queue.textChannel).catch(console.error);
                    message.react("✅")
 } catch (error) {
-        return sendError(`:notes: An unexpected error occurred.\nPossible type: ${error}`, message.channel);
+        return sendError(`:notes: Beklenmeyen bir hata oluştu.\nOlası tip: ${error}`, message.channel);
       }
   },
 };
