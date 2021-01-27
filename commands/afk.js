7cf90f55f3d59c6632f757fc409bd61a27426cb3
WhyAts2 <74931466+WhyAts2@users.noreply.github.com>
2021-01-27 15:44:13 +0300

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 const sendError = require("../util/error");
-const fs = require('fs');//youtube/NoblesYT
+const fs = require('fs');
 
 
 module.exports = {
   info: {
     name: "afk",
-    description: "24/7",//youtube/NoblesYT
+    description: "24/7",
     usage: "[afk]",
     aliases: ["24/7"],
   },
 
   run: async function (client, message, args) {
     let afk = JSON.parse(fs.readFileSync("./afk.json", "utf8"));
-       if (!afk[message.guild.id]) afk[message.guild.id] = {//youtube/NoblesYT
+       if (!afk[message.guild.id]) afk[message.guild.id] = {
         afk: false,
     };
     var serverQueue = afk[message.guild.id]
@@ -22,13 +22,13 @@ module.exports = {
              message.channel.send({
                 embed: {
                     color: "GREEN",
-                    description: `💤  **|**  AFK is **\`${serverQueue.afk === true ? "enabled" : "disabled"}\`**`//youtube/NoblesYT
+                    description: `💤  **|**  AFK **\`${serverQueue.afk === true ? "açıldı" : "kapatıldı"}\`**`
                 }
             });
             return  fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {
         if (err) console.error(err);
     });
         };
-    return sendError("There is nothing playing in this server.", message.channel);//youtube/NoblesYT
+    return sendError("Bu sunucuda oynatılan hiçbir şey yok.", message.channel);
   },
 };
