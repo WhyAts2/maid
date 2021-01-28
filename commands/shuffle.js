9f723a04931646c41febdc46d8ed42b35365277e
WhyAts2 <74931466+WhyAts2@users.noreply.github.com>
2021-01-28 12:59:41 +0300

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -4,14 +4,14 @@ const sendError = require("../util/error");
 module.exports = {
   info: {
     name: "shuffle",
-    description: "Shuffle queue",
+    description: "Sırayı karıştırır",
     usage: "[shuffle]",
     aliases: ["shuffle"],
   },
 
   run: async function (client, message, args) {
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is no queue.",message.channel).catch(console.error);
+    if (!serverQueue) return sendError("Şarkı çalma sırası yok.",message.channel).catch(console.error);
 try{
     let songs = serverQueue.songs;
     for (let i = songs.length - 1; i > 1; i--) {
@@ -24,7 +24,7 @@ try{
       } catch (error) {
         message.guild.me.voice.channel.leave();
         message.client.queue.delete(message.guild.id);
-        return sendError(`:notes: The player has stopped and the queue has been cleared.: \`${error}\``, message.channel);
+        return sendError(`:notes: Kullanıcı çıktı ve sıra temizlendi.: \`${error}\``, message.channel);
      }
   },
 };
